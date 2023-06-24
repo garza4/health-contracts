0825213de86fb33bf4c818a2029fdf02d038abb3
Bobby Garza <garzarj904@gmail.com>
2023-06-24 12:55:35 -0500

    payment algo done...

--- health-contracts/contracts/src/main/java/com/health/contracts/smart/AccountMangager.java
+++ health-contracts/contracts/src/main/java/com/health/contracts/smart/AccountMangager.java
@@ -7,14 +7,12 @@ import com.health.contracts.model.PaymentDecisions;
 import com.health.contracts.repository.ReceiptRepository;
 import com.health.contracts.service.impl.AccountImpl;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.stellar.sdk.responses.AccountResponse.Balance;
 
 /**
  * This class will help manage the funds in the master account.
@@ -26,19 +24,28 @@ import org.stellar.sdk.responses.AccountResponse.Balance;
 public class AccountMangager {
     private final AccountImpl accountImpl;
     private final ReceiptRepository receiptRepository;
-    private void manageMasterAccount(){
+    @Value("${stellar.master.acct}") private final String account;
+    public PaymentDecisions manageMasterAccount(){
+        var finalDecision = new PaymentDecisions(new ArrayList<>(),new ArrayList<>());
         try{
-            BalanceList balance = accountImpl.checkBalance("750000000");
+            BalanceList balance = accountImpl.checkBalance(account);
             List<FundReceiptEntity> receipts = receiptRepository.getPendingReceipts();
             String accountBalance = balance.getBalances().get(0).getBalance();
-            PaymentDecisions decisions = optimizePayments(receipts,accountBalance);
-            decisions.getCanPay().forEach(entity->entity.setStatus("R"));
-            receiptRepository.saveAll(decisions.getCanPay());
+            
+            //payments that can be made right off the bat...
+            var initialDecisions = optimizePayments(receipts,accountBalance);
+            //best payments from cant pay list
+            var secondaryDecisions = optimizePayments(initialDecisions.getCantPay(),accountBalance);
+            finalDecision.getCanPay().addAll(secondaryDecisions.getCanPay());
+            finalDecision.getCanPay().addAll(initialDecisions.getCanPay());
+            finalDecision = optimizePayments(finalDecision.getCanPay(),accountBalance);
+            
+            finalDecision.getCanPay().forEach(entity->entity.setStatus("R"));
+            receiptRepository.saveAll(finalDecision.getCanPay());
         }catch(Exception e){
             log.error("error managing account",e);
-        }
-        
-        
+        } 
+        return finalDecision;
     }
     /**
      * prioritize higher payments and separate into lists of can pay and cant pay
@@ -46,45 +53,51 @@ public class AccountMangager {
      * @param accountBalance 
      */
     private PaymentDecisions optimizePayments(List<FundReceiptEntity> receipts, String accountBalance){
-        PaymentDecisions paymentDecisions = new PaymentDecisions(new ArrayList<FundReceiptEntity>(),new ArrayList<FundReceiptEntity>());
-
+        var paymentDecisions = new PaymentDecisions(new ArrayList<>(),new ArrayList<>());
+        var toPay = new ArrayList<FundReceiptEntity>();
+        var cantPay = new ArrayList<FundReceiptEntity>();
+        var copyOfReceipts = new ArrayList<FundReceiptEntity>(receipts);
         try{
-            String totalOfPendingRequests = getAllPendingRequestAmounts(receipts);
+            String totalOfPendingRequests = getAllPendingRequestAmounts(copyOfReceipts);
             //sort based on amount
-            receipts.sort(Comparator.comparing(FundReceiptEntity::getTransferAmount));
+            copyOfReceipts.sort(Comparator.comparing(FundReceiptEntity::getTransferAmount));
             if(Integer.valueOf(totalOfPendingRequests) < Integer.valueOf(accountBalance)){
-                paymentDecisions.getCanPay().addAll(receipts);
+                paymentDecisions.getCanPay().addAll(copyOfReceipts);
             }else{
                 int sum = 0;
-                var toPay = new ArrayList<FundReceiptEntity>();
-                var cantPay = new ArrayList<FundReceiptEntity>();
-                int endOfReceipts = receipts.size()-1;
-                while(sum < Integer.valueOf(accountBalance)){
-                    if(endOfReceipts ==0){
-                        sum += Integer.valueOf(receipts.get(endOfReceipts).getTransferAmount());
+                
+                int endOfReceipts = copyOfReceipts.size()-1;
+                while(sum <= Integer.parseInt(accountBalance)){
+                    if(copyOfReceipts.size() == 0) break;
+                    if(copyOfReceipts.size()== 1){
+                        sum += Integer.valueOf(copyOfReceipts.get(endOfReceipts).getTransferAmount());
                         if(!sumGreaterThanBalance(sum,accountBalance)){
-                            toPay.add(receipts.get(endOfReceipts));
+                            toPay.add(copyOfReceipts.get(endOfReceipts));
                         }else{
-                            sum -= Integer.valueOf(receipts.get(endOfReceipts).getTransferAmount());
-                            cantPay.add(receipts.get(endOfReceipts));
+                            sum -= Integer.valueOf(copyOfReceipts.get(endOfReceipts).getTransferAmount());
+                            cantPay.add(copyOfReceipts.get(endOfReceipts));
                             //will be empty list here
-                            receipts.remove(endOfReceipts);
+                            copyOfReceipts.remove(endOfReceipts);
                         }
                         break;
                     }
-                    sum += Integer.valueOf(receipts.get(endOfReceipts).getTransferAmount());
-                    toPay.add(receipts.get(endOfReceipts));
+                    sum += Integer.valueOf(copyOfReceipts.get(endOfReceipts).getTransferAmount());
+                    toPay.add(copyOfReceipts.get(endOfReceipts));
                     if(sumGreaterThanBalance(sum,accountBalance)){
-                        sum -= Integer.valueOf(receipts.get(endOfReceipts).getTransferAmount());
+                        sum -= Integer.valueOf(copyOfReceipts.get(endOfReceipts).getTransferAmount());
                         toPay.remove(toPay.size()-1);
-                        cantPay.add(receipts.get(endOfReceipts));
-                        //cant pay values are in cant pay list
-                        receipts.remove(endOfReceipts);
-                        endOfReceipts = receipts.size()-1;
+                        cantPay.add(copyOfReceipts.get(endOfReceipts));
                     }
+                    //remove assesed receipt from copy list
+                    copyOfReceipts.remove(endOfReceipts);
+                    endOfReceipts = copyOfReceipts.size()-1;
                     endOfReceipts--;
                 }
             }
+            //after assesement add all to respective lists
+            paymentDecisions.getCanPay().addAll(toPay);
+            paymentDecisions.getCantPay().addAll(cantPay);
+            paymentDecisions.getCantPay().addAll(copyOfReceipts);
             
         }catch(Exception e){
             log.error("could not optimize",e);
