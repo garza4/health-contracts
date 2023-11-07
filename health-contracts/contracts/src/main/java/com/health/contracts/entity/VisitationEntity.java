d0a9ae8dffff26ba60b894f1f9b876d0c3d017e5
Bobby Garza <garzarj904@gmail.com>
2023-11-07 13:29:41 -0600

    add all args constructor

--- health-contracts/contracts/src/main/java/com/health/contracts/entity/VisitationEntity.java
+++ health-contracts/contracts/src/main/java/com/health/contracts/entity/VisitationEntity.java
@@ -1,13 +1,12 @@
 package com.health.contracts.entity;
 
-import java.math.BigInteger;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +18,7 @@ import lombok.Setter;
 @Setter
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class VisitationEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
